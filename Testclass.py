25694a94b6c1f041c28b52c565a33b1517c399af
divyasai439 <divyasai439@gmail.com>
2020-03-11 21:16:06 -0400

    Add files via upload

---
+++ Testclass.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Animal:
+    """A simple attempt to model a dog"""
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        print('Successfuly executed called out and intiated')
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(f"{self.name} is now sitting")
+    def roll_over(self):
+        """Simulate a dog roll over in response to command"""
+        print(f"{self.name} rolled over")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
